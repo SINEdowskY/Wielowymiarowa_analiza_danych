5fe15ceae56d474fdb0e751f38b5347468186bf2
SINEdowskY <kub200105@gmail.com>
2024-05-08 18:10:28 +0200

    preprocess and data import

--- import/import_data.py
+++ import/import_data.py
@@ -1,6 +1,23 @@
-import sqlalchemy
+from sqlalchemy import create_engine
 import pandas as pd
+import os
+
+server = 'localhost'
+database = 'Data'
+username = 'SA'
+password = 'YourStrongPassword123'
+driver = 'ODBC Driver 17 for SQL Server'
+
+# Tworzenie silnika SQLAlchemy
+engine = create_engine(f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}')
 
 
-class ImportData:
-    pass
+path = "data/preprocessed_data/csv/"
+files = os.listdir(path)
+print(files)
+
+for file in files:
+    chunks = pd.read_csv(path+file, chunksize=1000)
+    for chunk in chunks:
+        df = chunk.iloc[:, 1:]
+        df.to_sql("ARProperties", engine, if_exists='append', index=False)
