3503adcdebceaf9bbfcd8540c34dd57a0d5a3ca8
SINEdowskY <kub200105@gmail.com>
2024-05-13 07:34:46 +0200

    change database name

--- admin/create_database_system.py
+++ admin/create_database_system.py
@@ -3,4 +3,4 @@ from database import DatabaseAdmin
 db = DatabaseAdmin("SA", "YourStrongPassword123", "master", "localhost")
 
 db.create_database("Data")
-db.create_database("DataAgg")
+db.create_database("Data_Clean")
